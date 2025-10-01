b11395af3a05bb86f30508b0e860abbd4acff841
TarekHayan <tarekhayan77@gmail.com>
2025-10-01 18:30:08 +0300

    edit icon and splash screen

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:news_app/views/HomePadge.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -10,11 +14,11 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // بعد 3 ثواني يروح للصفحة الرئيسية
-    Future.delayed(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 3), () {
+      // هنا تروح للصفحة الرئيسية بعد السبلاتش
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomePadge()),
+        MaterialPageRoute(builder: (context) => const HomePadge()),
       );
     });
   }
@@ -22,34 +26,37 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // الخلفية
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // اللوجو
+            // أيقونة البرنامج
             Image.asset(
-              "assets/playstore.png", // حط صورتك هنا
+              'assets/1024.png', // ضع صورة الأيقونة هنا في مجلد assets
               width: 120,
               height: 120,
             ),
-            const SizedBox(height: 5),
-            // الاسم
+            const SizedBox(height: 30),
+
+            // اسم التطبيق
             Text(
-              "News Cloud", // اسمك
+              "News App",
               style: TextStyle(
-                color: Color(0xffcbff56), // اللون الفسفوري
-                fontSize: 22,
+                fontSize: 28,
                 fontWeight: FontWeight.bold,
+                color: Colors.greenAccent.shade400, // فسفوري
               ),
             ),
-            SizedBox(height: 100),
+            const SizedBox(height: 10),
+
+            // اسم المطور
             Text(
-              "By : Tarek Hayan", // اسمك
-              style: TextStyle(
-                color: Colors.white.withOpacity(0.4), // اللون الفسفوري
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+              "Developed by Tarek Hayan",
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Colors.white70,
               ),
             ),
           ],
